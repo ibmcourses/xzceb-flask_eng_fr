4e347e8af515800cd8eae85c79bec878b70e333d
ibmcourses <ibmcourses@xolw.com>
2022-12-15 23:07:32 -0500

    Final changes

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -7,55 +7,38 @@ import translator  # pylint: disable=import-error
 
 
 class TestTranslator(unittest.TestCase):
-    """
-    Tests for translator service
-    """
+    "Test translator service"
+
     def test_fr_to_en_bonjour(self):
-        """
-        Checks correct French to English translation of Bonjour
-        """
+        " Checks correct French to English translation of Bonjour "
         self.assertEqual(translator.french_to_english('Bonjour'), 'Hello')
 
     def test_fr_to_en_hello(self):
-        """
-        Checks that French to English translation of Hello is not Bonjour
-        """
+        " Checks that French to English translation of Hello is not Bonjour "
         self.assertNotEqual(translator.french_to_english('Hello'), 'Bonjour')
 
     def test_fr_to_en_empty(self):
-        """
-        Checks correct handling of empty strings
-        """
+        " Checks correct handling of empty strings "
         self.assertEqual(translator.french_to_english(''), '')
 
     def test_fr_to_en_none(self):
-        """
-        Checks correct handling of None value
-        """
+        " Checks correct handling of None value "
         self.assertEqual(translator.french_to_english(None), None)
 
     def test_en_to_fr_hello(self):
-        """
-        Checks correct English to French translation of Hello
-        """
+        " Checks correct English to French translation of Hello "
         self.assertEqual(translator.english_to_french('Hello'), 'Bonjour')
 
     def test_en_to_fr_bonjour(self):
-        """
-        Checks that English to French translation of Bonjour is not Hello
-        """
+        " Checks that English to French translation of Bonjour is not Hello "
         self.assertNotEqual(translator.english_to_french('Bonjour'), 'Hello')
 
     def test_en_to_fr_empty(self):
-        """
-        Checks correct handling of empty strings
-        """
+        " Checks correct handling of empty strings "
         self.assertEqual(translator.english_to_french(''), '')
 
     def test_en_to_fr_none(self):
-        """
-        Checks correct handling of None value
-        """
+        " Checks correct handling of None value "
         self.assertEqual(translator.english_to_french(None), None)
 
 
